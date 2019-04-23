d926d666ae66d910b9627c4860f69a17b4fcbaed
Emanuel4168 <emanuel.l4168@gmail.com>
2019-04-23 16:46:16 -0600

    Creacion de la clase MainView

---
+++ Aeropuerto/src/aeropuerto/MainView.java
@@ -0,0 +1,13 @@
+package aeropuerto;
+
+import javax.swing.*;
+
+public class MainView extends JFrame{
+	
+	public MainView() {
+		super("Aeropuerto");
+		setSize(800,600);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
